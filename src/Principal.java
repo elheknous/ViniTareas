3048db4ad7d28c0ec8838f84f2eb6f85fdecf59d
jose <josematustoro6@gmail.com>
2023-03-17 19:40:24 -0300

    Buscar canciones del artista solicitado si es que existe, total vinilos y total de espacio

--- src/Principal.java
+++ src/Principal.java
@@ -1,14 +1,65 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 public class Principal {
+    static Scanner sc = new Scanner(System.in);
+    static int contador;
     public static void main(String[] args) {
         //hola
+        Scanner sc = new Scanner(System.in);
         String[][] vinilos = new String[100][3];
+        System.out.println("Espacio maximo disponible: 100 vinilos");
+        System.out.println(" ");
         agregarVinilos(vinilos,"Iron maden","Iron maden","1980");
         agregarVinilos(vinilos,"Iron maden","Killers","1981");
         agregarVinilos(vinilos,"Iron maden","The Number Of The Beast","1982");
-
+        agregarVinilos(vinilos,"AC-DC","Back In Black","1980");
+        agregarVinilos(vinilos,"Kiss","Destroyer","1976");
+        totalVinilos(vinilos);
+        buscarArtista(vinilos);
         imprimirMatriz(vinilos,100,3);
 
+    }
 
+    private static void buscarArtista(String[][] vinilos) {
+        ArrayList<String> cancionesEncontradas = new ArrayList<>();
+        System.out.println("Que artista desea buscar: ");
+        String artista = sc.nextLine();
+        artista = artista.toUpperCase();
+        Boolean existen = false;
+
+        for (int i = 0; i < contador; i++) {
+            String aux = vinilos[i][0];
+            if(aux.toUpperCase().equals(artista)){
+                cancionesEncontradas.add(vinilos[i][1] +" - "+vinilos[i][2]);
+                existen = true;
+            }
+        }
+
+        if (existen){
+            System.out.println("El artista existe");
+            System.out.println("Las canciones encontradas del artista son: ");
+            for (String i : cancionesEncontradas){
+                System.out.println(i);
+            }
+        }else {
+            System.out.println("Lo sentimos, no hay canciones de ese artista:(");
+        }
+        System.out.println(" ");
+
+
+    }
+
+    private static void totalVinilos(String[][] vinilos) {
+        contador = 0;
+        for (int i = 0; i < 100; i++) {
+            if (vinilos[i][0] != null){
+                contador++;
+            }
+
+        }
+        System.out.println("Hay un total de "+contador+" vinilos");
+        System.out.println("Hay "+(100-contador)+" espacios disponibles");
+        System.out.println(" ");
     }
 
     private static void agregarVinilos(String[][] vinilos, String banda, String disco, String cancion) {
@@ -35,7 +86,7 @@ public class Principal {
     }
 
     private static void imprimirMatriz(String[][] matriz, int filas, int columnas) {
-
+        System.out.println("Los vinilos disponibles son: ");
         for (int i = 0; i < filas; i++) {
             if (matriz[i][0] != null) {
                 for (int j = 0; j < columnas; j++) {
@@ -47,3 +98,4 @@ public class Principal {
         }
     }
 }
+
